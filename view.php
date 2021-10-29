cf50cb2b29aa6ee9b063518d97039f688c7de88e
LeonLit <leonlit123@gmail.com>
2021-10-29 17:05:43 +0800

    added some php code for the xml url for The Star

--- view.php
+++ view.php
@@ -1,15 +1,39 @@
 <?php
     $view = $_GET["view"];
+    
+    $base_url = "https://www.thestar.com.my/rss/";
     $accepted_views = array("news", "sport", "metro", "education", "businesss", "tech", "opinion");
-    $news_rss = array("/", "/Nation", "/Regional", "/World", "/Environment", "/In%20Other%20Media", "/True%20Or%20Not");
-    $business_rss = array("/", "/Business%20News", "/SMEBiz");
+    $news_rss = array("/", "/Nation", "/Regional", "/World", "/Environment", "/In Other Media", "/True Or Not");
+    $business_rss = array("/", "/Business News", "/SMEBiz");
     $sport_rss = array("/", "/Football", "/Golf", "/Badminton", "/Tennis", "/Motorsport");
-    $metro_rss = array("/", "/Metro%20News", "/Eat%20And%20Drink", "/Focus", "/Views");
-    $tech_rss = array("/", "/Tech%20News", "/Reviews", "/Games");
+    $metro_rss = array("/", "/Metro News", "/Eat And Drink", "/Focus", "/Views");
+    $tech_rss = array("/", "/Tech News", "/Reviews", "/Games");
     $opinion_rss = array("/", "/Columnists", "/Letters");
     $education_rss = array("/");
 
-    if (in_array($view, $accepted_views)) {
+    if ($view === "news") {
+        $sub_base = "News";
+        //foreach ($news_rss as $section) {
+            create_section($sub_base, $news_rss[0]);
+        //}
+    }
 
+    function create_section ($sub_base, $section) {
+        $content = simplexml_load_file($GLOBALS["base_url"].$sub_base.$section) or die("Error: Cannot create object");
+        $container_rss = $content->channel->item[0];
+        //var_dump($container_rss);
+        echo $container_rss-> lead;
+        /* for ($count = 0; $count < 10; $count++) {
+            $container_rss = $content->channel->item[$count];
+            $premium = $container_rss->premium;
+            $exclusive = $container_rss->isexclusive;
+            $link = $container_rss->link;
+            $description = $container_rss->description
+            $title = $container_rss-> title;
+            $date = $container_rss -> pubDate
+            $lead = $container_rss -> lead
+            $tags = $container_rss -> section
+
+        } */
     }
 ?>
