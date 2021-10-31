6dfb28e8c0c1c47a8c28ef8227e2edcdde32d911
LeonLit <leonlit123@gmail.com>
2021-10-31 08:38:28 +0800

    fixed nav for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,17 @@ function open_close_nav() {
         isNavOpen = false;
     }
 }
+
+const dropdown = document.getElementsByClassName("dropdown");
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        const dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
