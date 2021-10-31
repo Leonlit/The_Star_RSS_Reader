dc7ccf06adc43b53e63f548fbc1da6f2d02e83c0
LeonLit <leonlit123@gmail.com>
2021-10-31 10:16:45 +0800

    fixed nav_bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,13 +13,29 @@ function open_close_nav() {
 const dropdown = document.getElementsByClassName("dropdown");
 
 for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+    dropdown[i].addEventListener("click", function(event) {
         const dropdownContent = this.nextElementSibling;
         if (dropdownContent.style.display === "block") {
             dropdownContent.style.display = "none";
         } else {
             dropdownContent.style.display = "block";
         }
+        let id = "" + event.target.id;
+        console.log(typeof(id));
+        id = id.split("-")[1];
+        console.log(id);
+        remove_other_dropdown_content_view(id);
     });
+}
+
+function remove_other_dropdown_content_view(exceptionEle) {
+    const dropdownContent = document.getElementsByClassName("dropdown_content");
+    for (i = 0; i < dropdownContent.length; i++) {
+        console.log(exceptionEle, i);
+        if (i == exceptionEle) {
+            continue
+        }else {
+            dropdownContent[i].style.display = "none";
+        }
+    }
 }
